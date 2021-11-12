56113381a5e36d1e3444c1fe1dea3fee7fb66cc6
vgcaccamo <vgcaccamo@gmail.com>
2021-11-12 16:44:40 -0500

    validation on registration fields

--- CryptoCapitol/src/app/components/register/register.component.ts
+++ CryptoCapitol/src/app/components/register/register.component.ts
@@ -5,34 +5,18 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  userName!: string;
-  password!: string;
-  firstName!: string;
-  lastName!: string;
-  email!: string;
+  userData = new UserDTO('', '', '', '', '');
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmit() {
-    const newUser = {
-      userName: this.userName,
-      password: this.password,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      email: this.email
-    };
-    this.registerUser(newUser);
-    this.userName = '';
-    this.password = '';
-    this.firstName = '';
-    this.lastName = '';
-    this.email = '';
+  onSubmit(form: any, formData: any) {
+    this.registerUser(formData);
+    form.reset();
   }
 
   registerUser(user: UserDTO) {
